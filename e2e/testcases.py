399df65577964dc9b8c1c53abc5d51e56bab8921
marcsello <punkosdmarcell@rocketmail.com>
2023-04-11 00:33:42 +0200

    tests

--- e2e/testcases.py
+++ e2e/testcases.py
@@ -516,6 +516,27 @@ class LongPollBasic(TestCaseBase):
         time.sleep(1)
 
         r = self.request_and_expect_status("POST", "/api/post", 201, json=post)
+        post_1 = r.json()
 
         t.join()
-        expect_json_tree(r.json(), polled_r.json())
+        assert len(polled_r.json()) == 1
+        expect_json_tree(post_1, polled_r.json()[0])
+
+        r = self.request_and_expect_status("POST", "/api/post", 201, json=post)
+        post_2 = r.json()
+
+        r = self.request_and_expect_status("GET", "/api/poll?last=0", 200)
+        assert len(r.json()) == 2
+        expect_json_tree([post_1, post_2], r.json())
+
+        r = self.request_and_expect_status("GET", "/api/poll?last=1", 200)
+        assert len(r.json()) == 1
+        expect_json_tree([post_2], r.json())
+
+        got_exc = False
+        try:
+            self.request_and_expect_status("GET", "/api/poll?last=2", 200, timeout=5)
+        except requests.exceptions.Timeout:
+            got_exc = True
+
+        assert got_exc
